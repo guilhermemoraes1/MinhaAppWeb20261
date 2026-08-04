2622f5a4639e15ed712230d919e131e75e29f0b3
Guilherme Moraes <127672759+guilhermemoraes1@users.noreply.github.com>
2026-08-04 12:05:03 -0300

    corrigindo os testes

--- src/test/views/InstituicaoEnsino.test.jsx
+++ src/test/views/InstituicaoEnsino.test.jsx
@@ -3,32 +3,19 @@ import '@testing-library/jest-dom';
 import InstituicaoEnsino from '../../views/InstituicaoEnsino';
 
 describe('InstituicaoEnsino', () => {
-  test('deve adicionar uma instituição ao submeter o formulário', async () => {
-    const { container } = render(<InstituicaoEnsino />);
+  test('deve adicionar uma instituição ao clicar no botão Adicionar', () => {
+    render(<InstituicaoEnsino />);
 
     // Inicialmente a instituição não deve existir
     expect(
       screen.queryByText('IFPB - Campus João Pessoa'),
     ).not.toBeInTheDocument();
 
-    // Preenche o formulário
-    fireEvent.change(container.querySelector('#nome'), {
-      target: { value: 'IFPB - Campus João Pessoa' },
-    });
-    fireEvent.change(container.querySelector('#codigo'), {
-      target: { value: '1000' },
-    });
-    fireEvent.change(container.querySelector('#qtMatricula'), {
-      target: { value: '600' },
-    });
-
-    // Submete o formulário
-    fireEvent.click(screen.getByRole('button', { name: /enviar/i }));
+    // Clica no botão
+    fireEvent.click(screen.getByRole('button', { name: /adicionar/i }));
 
     // Agora a instituição deve aparecer na tabela
-    expect(
-      await screen.findByText('IFPB - Campus João Pessoa'),
-    ).toBeInTheDocument();
+    expect(screen.getByText('IFPB - Campus João Pessoa')).toBeInTheDocument();
 
     expect(screen.getByText('1000')).toBeInTheDocument();
     expect(screen.getByText('600')).toBeInTheDocument();
